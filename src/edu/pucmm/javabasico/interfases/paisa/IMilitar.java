0887f9ed9a3d622fd6d39e4520ff1d23e2346950
erquier <ernest324@gmail.com>
2018-08-10 08:24:14 -0400

    actualizando datos

---
+++ src/edu/pucmm/javabasico/interfases/paisa/IMilitar.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.pucmm.javabasico.interfases.paisa;
+
+/**
+ *
+ * @author vacax
+ */
+public interface IMilitar {
+    
+    //Todas las propiedades una interfaz son constantes, publicas, estaticas y final.
+    public static final int CONSTANTE = 1;
+    int CONSTANTE2 = 2; //de forma implicita está el public static final....
+    
+    //Todos los metodos son abstractros.
+    public abstract void disparar();
+    
+    void seguirOrdenes(); // el publico y abstracto está implicito....
+   
+    default void entrenar(){ //agregando default puedo implementar java 8 en adelante.
+        System.out.println("Entrenado.... "+ this.getClass().getName());
+    }
+}
